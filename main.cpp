58e63c2154afe056649818babe359c63e56af8d0
AlessandroRotondi <102591089+AlessandroRotondi@users.noreply.github.com>
2022-08-27 11:11:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 
 #include "flock.cpp"
+#include "graphic.cpp"
 #include "vettore.cpp"
-#include "Graphic.cpp"
 
 int main() {
   int n;
   std::cout << "insert the number of birds you want in the flock" << '\n';
   std::cin >> n;
-  const int display_height=650;
-  const int display_width=1200;
+  const int display_height = 650;
+  const int display_width = 1200;
   game gioco;
   gioco.run(n);
   return 0;
