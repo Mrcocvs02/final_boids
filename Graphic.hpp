adc3232e95fce2ef2656e325c7c29d53fa8f4305
AlessandroRotondi <102591089+AlessandroRotondi@users.noreply.github.com>
2022-08-28 13:30:18 +0200

    Update Graphic.hpp

--- Graphic.hpp
+++ Graphic.hpp
@@ -1,35 +1,31 @@
 #include <iostream>
 #include <vector>
 
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
 #include "flock.hpp"
 #include "vettore.hpp"
-#include "SFML/Window.hpp"
-#include "SFML/Graphics.hpp"
 
- #ifndef GRAPHIC_HPP
- #define GRAPHIC_HPP
+#ifndef GRAPHIC_HPP
+#define GRAPHIC_HPP
 
 class game {
-    private:
-    sf::RenderWindow window;
-    const int fps;
-    const int display_height;
-    const int display_width;
-    bool play_check = true;
-    
+ private:
+  sf::RenderWindow window;
+  const int fps = 30;
+  const int display_height = 650;
+  const int display_width = 1200;
+  bool play_check = true;
 
-    public:
-    game():
-    display_height{650}, display_width{1200}, fps{30}{}
-    void run(int n);
-    void close (sf::RenderWindow& window);
-    
-    bool get_play_check(){return play_check;}
-    void set_play_check(bool b){play_check=b;}
-    void play(flock& flock);
+ public:
+  void run(flock& Flock);
+  void close_window();
 
+  bool get_play_check();
+  void set_play_check(bool b);
+  void play(flock& flock);
 };
 inline float angle();
-inline void predator_movement(sf::CircleShape& predator_shape, flock& Flock);
+inline void predator_movement(flock& Flock);
 
 #endif
